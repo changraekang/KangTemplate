07d5f1ff673e383f28b3fe0e5eb1436a12990d0b
Changrae <storkcr@gmail.com>
2022-03-24 18:41:50 +0900

    Kang template 초기 설정

---
+++ models/User.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    uName:{
+        type: String,
+        maxlength: 50
+    },
+    uEmail:{
+        type: String,
+        trim: true,
+        unique:1
+    },
+    uPassword:{
+        type: String,
+        minlength: 5
+    },
+    uLastname:{
+        type: String,
+        maxlength: 50
+    },
+    uRole:{
+        type: Number,
+        default: 0
+    },
+    uImage:{
+        type: String,
+    },
+    uTokenExp:{
+        type: Number,
+    },
+})
+
+const User = mongoose.model( 'User', userSchema)
+
+module.exports = { User };
