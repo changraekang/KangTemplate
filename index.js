11c8a4a7a13c62ed33edb9d8a7164eccc720aae4
Changrae <storkcr@gmail.com>
2022-03-24 19:00:16 +0900

    MongoDB 통신

--- index.js
+++ index.js
@@ -2,6 +2,12 @@
  const mongoose  = require("mongoose");
  const app      = express()
  const port     = 5000
+ const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+const { User } = require('./models/User');
+
  mongoose.connect(
    "mongodb+srv://kangchangrae:l8IndR6tl0I5I24e@cluster0.2njz4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
    {
@@ -12,5 +18,20 @@
 
 
  app.get('/',(req, res) => res.send('Hello World!!'))
+
+ app.post('/register', (req, res) => {
+
+
+
+    const user = new User(req.body)
+    user.save((err, userInfo) => {
+        if(err) return res.json({ success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+
+ })
+
 
  app.listen(port, ()=> console.log(`Example app listening on port ${port}!`))
