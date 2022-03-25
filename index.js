209bfd8e4435ff1c58bcf317eae7319d7e68dc46
Changrae <storkcr@gmail.com>
2022-03-25 13:26:57 +0900

    key 분리 및 숨기기

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
- const express  = require('express')
- const mongoose  = require("mongoose");
- const app      = express()
- const port     = 5000
- const bodyParser = require('body-parser');
+const express  = require('express')
+const mongoose  = require("mongoose");
+const app      = express()
+const port     = 5000
+const bodyParser = require('body-parser');
+const config = require('./config/key');
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 const { User } = require('./models/User');
 
- mongoose.connect(
-   "mongodb+srv://kangchangrae:l8IndR6tl0I5I24e@cluster0.2njz4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-   {
+ mongoose
+   .connect(config.MongoURI, {
      useNewUrlParser: true,
      useUnifiedTopology: true,
-   }
- ).then(()=>console.log("mongoDB Connected")).catch( err => console.log(err))
+   })
+   .then(() => console.log("mongoDB Connected"))
+   .catch((err) => console.log(err));
 
 
  app.get('/',(req, res) => res.send('Hello World!!'))
