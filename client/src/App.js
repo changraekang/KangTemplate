667f594d62cd80aa2b3ae5b46dbd772ae37b2b8c
Changrae <storkcr@gmail.com>
2022-04-06 13:34:38 +0900

    react-router-dom 설정

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import LoginPage from './components/views/LoginPage/LoginPage';
+import RegisterPage from './components/views/RegisterPage/RegisterPage';
+import Home from './Home';
 function App() {
   return (
-    <div className="App">
-     <h1>강창래</h1>
-    </div>
+    <Router>
+      <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<RegisterPage />} />
+          <Route path={process.env.PUBLIC_URL + "/"} element={<Home />} />
+        </Routes>
+    </Router>
   );
 }
 
