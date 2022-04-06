60f6a4364f635ff787c3a28f878b33a7546c772c
Changrae <storkcr@gmail.com>
2022-04-06 18:18:49 +0900

    loginpage 만들기

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import 'antd/dist/antd.css';
+import { applyMiddleware, createStore } from 'redux';
+import promiseMiddleware  from 'redux-promise'
+import ReduxThunk from 'redux-thunk'
+const createStoreWithMiddleware = applyMiddleware( promiseMiddleware , ReduxThunk)(createStore)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+<Provider>
+  <App />
+</Provider>
+
+,document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
